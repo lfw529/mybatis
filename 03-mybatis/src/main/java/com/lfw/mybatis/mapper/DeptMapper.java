8c01d2dbb0cb27134afc2f3c184ed9da192044f2
lfw529 <differentlee1012@163.com>
2022-06-23 10:49:35 +0800

    111

--- 03-mybatis/src/main/java/com/lfw/mybatis/mapper/DeptMapper.java
+++ 03-mybatis/src/main/java/com/lfw/mybatis/mapper/DeptMapper.java
@@ -6,7 +6,8 @@ import org.apache.ibatis.annotations.Param;
 public interface DeptMapper {
 
   /**
-   * 通过分步查询查询员工以及员工所对应的部门信息 分步查询第二步：通过did查询员工所对应的部门
+   * 通过分步查询查询员工以及员工所对应的部门信息
+   * 分步查询第二步：通过did查询员工所对应的部门
    */
   Dept getEmpAndDeptByStepTwo(@Param("did") Integer did);
 
--- 03-mybatis/src/main/java/com/lfw/mybatis/mapper/EmpMapper.java
+++ 03-mybatis/src/main/java/com/lfw/mybatis/mapper/EmpMapper.java
@@ -17,7 +17,8 @@ public interface EmpMapper {
   Emp getEmpAndDept(@Param("eid") Integer eid);
 
   /**
-   * 通过分布查询查询员工以及员工对应的部门信息 分布查询第一步：查询员工信息
+   * 通过分布查询查询员工以及员工对应的部门信息
+   * 分布查询第一步：查询员工信息
    */
   Emp getEmpAndDeptByStepOne(@Param("eid") Integer eid);
 }
